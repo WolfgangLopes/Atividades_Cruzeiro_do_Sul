5430697e2f0ea055fb8be48ede5438f4a1f3942d
WolfgangLopes <wfglopes@gmail.com>
2025-03-11 20:52:55 -0300

    Create Aluno.java

---
+++ Semestre2/Orientado_objeto/Atividade/Aluno.java
@@ -0,0 +1,11 @@
+package Exercicio2;
+
+public class Aluno{
+    String nome;
+    Double nota[];
+
+    Aluno(String nome, Double nota[]){
+        this.nome = nome;
+        this.nota = nota;
+    }    
+}
