f76cdefc5ff22d95ba0cc191430babfb6ea20d9c
WolfgangLopes <wfglopes@gmail.com>
2025-03-28 20:56:17 -0300

    Create Conta.java

---
+++ Semestre2/Orientado_objeto/Aula05/Conta.java
@@ -0,0 +1,34 @@
+package aula05;
+import javax.swing.JOptionPane;
+
+public class Conta {
+    String numero;
+    double saldo;
+
+    //Construtor
+    Conta(String n, double saldo){
+        numero=n;
+        this.saldo=saldo;
+    }
+
+    void imprimeDados(){
+        JOptionPane.showMessageDialog(null, "Dados de conta: "
+        +"\nNúmero: "+numero+"\nSaldo: "+saldo);
+    }
+    
+    void sacarValor(double valor){
+        if(valor>saldo){
+            JOptionPane.showMessageDialog(null,"Saldo insuficiente");
+        }else{
+            saldo=saldo-valor;
+        }
+    }
+    public double maiorSaldo(Conta c){
+        if(this.saldo>c.saldo){
+            return this.saldo;
+        }else{
+            return c.saldo;
+        }
+    }
+    
+}
