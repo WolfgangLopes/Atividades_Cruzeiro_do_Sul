cb180ea12f8c47940f0e77d240670e2e32a10024
Wolfgang Lopes <wfglopes@gmail.com>
2025-08-25 21:35:59 -0300

    Create server.js

---
+++ Semestre3/TopicosAvançados2/Aula03/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+let carrinho = [];
+
+app.get("/", (req,res) => {
+    res.render("index",{ carrinho });
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
