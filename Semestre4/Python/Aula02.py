d07cec388bde36af57e9b5eaa3c067598a7cf01b
Wolfgang Lopes <wfglopes@gmail.com>
2026-03-14 10:43:18 -0300

    Create Aula02.py

---
+++ Semestre4/Python/Aula02.py
@@ -0,0 +1,16 @@
+print("Calculo de média de notas\n")
+
+i=0
+j=i+1
+notafinal=0
+nota=[0]*4
+
+while(i<4):
+    print("Digite a nota "+str(j)+"/4:\n")
+    nota[i] = int(input(""))    
+    i=i+1
+
+media = (nota[0]+nota[1]+nota[2]+nota[3])/4
+
+print("Média das notas: ", media)
+
